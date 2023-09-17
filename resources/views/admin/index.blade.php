d2662420107ded90ba6352f4ac338a8ce5db9a0f
IrvinVJ <jhosepventurajavier@gmail.com>
2023-09-17 16:22:45 -0500

    first commit

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,77 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+<section class="section">
+    <div class="section-header">
+        <h3 class="page__heading">Dashboard</h3>
+    </div>
+    <div class="section-body">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body">                          
+                            <div class="row">
+                                <div class="col-md-4 col-xl-4">
+                                
+                                <div class="card order-card" style="background: linear-gradient(45deg,#73b4ff, #4099ff);">
+                                        <div class="card-block">
+                                        <h5>Usuarios</h5>                                               
+                                            @php
+                                             use App\Models\User;
+                                            $cant_usuarios = User::count();                                                
+                                            @endphp
+                                            <h2 class="text-right"><i class="fa fa-users f-left"></i><span>{{$cant_usuarios}}</span></h2>
+                                            <p class="m-b-0 text-right"><a href="/usuarios" class="text-white">Ver más</a></p>
+                                        </div>                                            
+                                    </div>                                    
+                                </div>
+                                
+                                <div class="col-md-4 col-xl-4">
+                                    <div class="card order-card" style="background: linear-gradient(45deg,#59e0c5, #2ed8b6);">
+                                        <div class="card-block">
+                                        <h5>Roles</h5>                                               
+                                            @php
+                                            use Spatie\Permission\Models\Role;
+                                             $cant_roles = Role::count();                                                
+                                            @endphp
+                                            <h2 class="text-right"><i class="fa fa-user-lock f-left"></i><span>{{$cant_roles}}</span></h2>
+                                            <p class="m-b-0 text-right"><a href="/roles" class="text-white">Ver más</a></p>
+                                        </div>
+                                    </div>
+                                </div>                                                                
+                                
+                                <div class="col-md-4 col-xl-4">
+                                    <div class="card order-card" style="background: linear-gradient(45deg,#ff869a, #FF5370);">
+                                        <div class="card-block">
+                                            <h5>Blogs</h5>                                               
+                                            @php
+                                             use App\Models\Blog;
+                                            $cant_blogs = Blog::count();                                                
+                                            @endphp
+                                            <h2 class="text-right"><i class="fa fa-blog f-left"></i><span>{{$cant_blogs}}</span></h2>
+                                            <p class="m-b-0 text-right"><a href="/blogs" class="text-white">Ver más</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\RolController;
+use App\Http\Controllers\UsuarioController;
+use GuzzleHttp\Middleware;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+Route::group(['middleware' => ['auth']], function(){
+    Route::resource('roles', RolController::class);
+    Route::resource('usuarios', UsuarioController::class);
+    Route::resource('blogs', BlogController::class);
+});
